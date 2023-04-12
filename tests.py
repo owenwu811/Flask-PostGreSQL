91a817c8e306545e7a0086d91e49e13a9325f507
Owen Wu <55355014+owenwu811@users.noreply.github.com>
2023-04-11 21:56:03 -0400

    Create tests.py
    This is a unit test for creating a user and updating an existing user. Note that that there will be more coverage to follow.

---
+++ tests.py
@@ -0,0 +1,45 @@
+import unittest
+from flask import json
+from app import app, db, User
+
+class TestUsers(unittest.TestCase):
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@172.30.112.1:5432/database1'
+        self.app = app.test_client()
+        self.context = app.app_context()
+        self.context.push()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        for user in User.query.all():
+            db.session.delete(user)
+        db.session.commit()
+        db.drop_all()
+        self.context.pop()
+
+    def test_create_user(self):
+        # Send a POST request to create a user
+        response = self.app.post('/users', data=json.dumps({'name': 'John', 'email': 'john@example.com'}), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(User.query.count(), 1)
+
+    def test_update_user(self):
+        # Create a user
+        user = User(name='John', email='john@example.com')
+        db.session.add(user)
+        db.session.commit()
+
+        # Send a PUT request to update the user
+        response = self.app.put(f'/users/{user.id}', data=json.dumps({'name': 'Jane'}), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+        # Check that the user was updated
+        updated_user = User.query.get(user.id)
+        self.assertEqual(updated_user.name, 'Jane')
+
+if __name__ == '__main__':
+    unittest.main()
+    
+#To execute, tests, run - python -m unittest discover -s tests - in the VSCode terminal
